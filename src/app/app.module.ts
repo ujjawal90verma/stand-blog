943e592d98a128d6441f34c64d84d4fc84047c1c
Ujjawal Verma <ujjawal90verma@gmail.com>
2021-03-31 14:31:12 +0530

    Made some changes to folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/core/header/header.component';
 import { FooterComponent } from './modules/core/footer/footer.component';
-import { CarouselComponent } from './modules/home/carousel/carousel.component';
-import { CarouselItemComponent } from './modules/home/carousel-item/carousel-item.component';
-import { TemplateBannerComponent } from './modules/shared/template-banner/template-banner.component';
-import { PostComponent } from './modules/shared/post/post.component';
-import { RecentPostsComponent } from './modules/shared/recent-posts/recent-posts.component';
-import { CategoriesComponent } from './modules/shared/categories/categories.component';
-import { TagCloudsComponent } from './modules/shared/tag-clouds/tag-clouds.component';
-import { PostSearchComponent } from './modules/shared/post-search/post-search.component';
-import { ModuleBannerComponent } from './modules/shared/module-banner/module-banner.component';
-import { AboutUsDetailComponent } from './modules/about-us/components/about-us-detail/about-us-detail.component';
-import { BriefDescriptionComponent } from './modules/about-us/components/brief-description/brief-description.component';
-import { YourCommentsComponent } from './modules/post-details/components/your-comments/your-comments.component';
-import { CommentComponent } from './modules/post-details/components/comment/comment.component';
-import { SendUsMessageComponent } from './modules/contact-us/components/send-us-message/send-us-message.component';
-import { ContactInformationComponent } from './modules/contact-us/components/contact-information/contact-information.component';
-import { AddressGeoMapComponent } from './modules/contact-us/components/address-geo-map/address-geo-map.component';
+import { CarouselComponent } from './modules/home/components/carousel/carousel.component';
+import { CarouselItemComponent } from './modules/home/components/carousel-item/carousel-item.component';
+import { HomeComponent } from './modules/home/home/home.component';
+import { AboutUsComponent } from './modules/about-us/about-us/about-us.component';
+import { BlogEntriesComponent } from './modules/blog-entries/blog-entries/blog-entries.component';
+import { ContactUsComponent } from './modules/contact-us/contact-us/contact-us.component';
+import { CoreComponent } from './modules/core/core/core.component';
+import { PostDetailsComponent } from './modules/post-details/post-details/post-details.component';
+import { SharedComponent } from './modules/shared/shared/shared.component';
+
 
 @NgModule({
   declarations: [
@@ -29,20 +23,14 @@ import { AddressGeoMapComponent } from './modules/contact-us/components/address-
     FooterComponent,
     CarouselComponent,
     CarouselItemComponent,
-    TemplateBannerComponent,
-    PostComponent,
-    RecentPostsComponent,
-    CategoriesComponent,
-    TagCloudsComponent,
-    PostSearchComponent,
-    ModuleBannerComponent,
-    AboutUsDetailComponent,
-    BriefDescriptionComponent,
-    YourCommentsComponent,
-    CommentComponent,
-    SendUsMessageComponent,
-    ContactInformationComponent,
-    AddressGeoMapComponent
+    HomeComponent,
+    AboutUsComponent,
+    BlogEntriesComponent,
+    ContactUsComponent,
+    CoreComponent,
+    PostDetailsComponent,
+    SharedComponent,
+    
   ],
   imports: [
     BrowserModule,
